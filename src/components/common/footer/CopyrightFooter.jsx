cac3bcc30292c1bc7e9daf3a425713c56668bdb3
Vasu <sharvasu7@gmail.com>
2023-01-20 15:58:30 +0530

    fixedisuues

--- src/components/common/footer/CopyrightFooter.jsx
+++ src/components/common/footer/CopyrightFooter.jsx
@@ -5,11 +5,13 @@ import CopyrightSocial from "./CopyrightSocial";
 const CopyrightFooter = () => {
   const footerList = [
     { id: 1, name: "Privacy & Terms.", routeLink: "/privacy-policy" },
-    { id: 2, name: " hello@answergenie.co" },
+    { id: 2, name: "Terms & Services.", routeLink: "/term-and-services" },
+
+    { id: 3, name: " hello@answergenie.co" },
   ];
   return (
     <div className="row">
-      <div className="col-lg-4 order-lg-0 mb-15">
+      <div className="col-lg-6 order-lg-0 mb-15">
         <ul className="d-flex justify-content-center justify-content-lg-start footer-nav style-none">
           {footerList.map((list) => (
             <li key={list.id}>
@@ -20,14 +22,14 @@ const CopyrightFooter = () => {
       </div>
       {/* End .col */}
 
-      <div className="col-lg-4 order-lg-2 mb-15">
+      <div className="col-lg-3 order-lg-2 mb-15">
         <ul className="d-flex justify-content-center justify-content-lg-end social-icon style-none">
           <CopyrightSocial />
         </ul>
       </div>
       {/* End .col */}
 
-      <div className="col-lg-4 order-lg-1 mb-15">
+      <div className="col-lg-3 order-lg-1 mb-15">
         <p className="copyright text-center">
           {" "}
           Answergenie {new Date().getFullYear()}
